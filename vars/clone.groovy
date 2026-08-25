0547becbae019377c7bdbc9dd63b4b8e80c63b55
ashishjaiswal97 <35159072+ashishjaiswal97@users.noreply.github.com>
2026-08-25 07:41:30 +0530

    Fix syntax for call function in clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,5 @@
-def call(string url, string branch)
+def call(string url, string branch){
     echo "This is coding the code"
     git url: $url, branch: $branch
     echo "Code is clonned successfully"
+}
